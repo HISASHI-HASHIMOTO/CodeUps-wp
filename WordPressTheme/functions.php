d0e94f62b6cc62cc32acac0401d38a1a55a61f51
hisashi1919 <hisanori951.81@gmail.com>
2024-06-29 23:01:22 +0900

    6/29

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -40,4 +40,16 @@ function codeups_add_meta_tags() {
     echo '<link rel="icon" href="" />';
 }
 add_action('wp_head', 'codeups_add_meta_tags');
+
+/**
+ * @param string $page_title ページのtitle属性値
+ * @param string $menu_title 管理画面のメニューに表示するタイトル
+ * @param string $capability メニューを操作できる権限（manage_options とか）
+ * @param string $menu_slug オプションページのスラッグ。ユニークな値にすること。
+ * @param string|null $icon_url メニューに表示するアイコンの URL
+ * @param int $position メニューの位置
+ */
+SCF::add_options_page( 'campaign__swiper', 'キャンペーンカード', 'manage_options', 'theme-options' );
+
+
 ?>
